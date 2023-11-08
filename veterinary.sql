a64f4aaf2bb8db9cdd9e87ab4d4bd1727b1d294a
BalmesClarenceDaven <Clarencebalmes29@gmail.com>
2023-11-08 12:28:40 +0800

    feat/list-animal-w-most-appointment

--- veterinary.sql
+++ veterinary.sql
@@ -167,3 +167,11 @@ FROM invoices
 SELECT COUNT(*) AS total_appointments
 FROM appointment
 WHERE animalid IN (SELECT animalid FROM owners WHERE ofirstname = 'Maria');
+
+--feat/list-animal-w-most-appoinment
+SELECT a.animalid, a.name, COUNT(*) AS appointment_count
+FROM animal a
+JOIN appointment ap ON a.animalid = ap.animalid
+GROUP BY a.animalid, a.name
+ORDER BY appointment_count DESC
+LIMIT 1;
